c2da9746658e8e44a1d701e60af4dc880b176e82
hikchi <abkcckab@protonmail.com>
2022-11-21 16:16:49 -0500

    chore: test goerli

--- test/v4/mine.test.ts
+++ test/v4/mine.test.ts
@@ -16,8 +16,8 @@ jest.setTimeout(300 * 1000);
 
 // sample transaction on ropsten: https://ropsten.etherscan.io/tx/0x316769bc70b7498a532f630f6025d43dde5d05d84d0ae422b149e4c6cc2fa54b
 // sample transaction on rposten(with fee): https://ropsten.etherscan.io/tx/0x0a69250e6c5dd046d455b742bf932488158f2b860574bb5bc5ae15d9919d3fc8
-// sample transaction on goerli: https://goerli.etherscan.io/tx/0x316769bc70b7498a532f630f6025d43dde5d05d84d0ae422b149e4c6cc2fa54b
-// sample transaction on goerli(with fee): https://goerli.etherscan.io/tx/0x0a69250e6c5dd046d455b742bf932488158f2b860574bb5bc5ae15d9919d3fc8
+// sample transaction on goerli: https://goerli.etherscan.io/tx/0x53b2f046fc8fbffb6e7a92047f462689097e984c0f56880dba65afb9a8a4a841
+// sample transaction on goerli(with fee): https://goerli.etherscan.io/tx/0xccdef383de24b28196310116c9021d33f86d352180dc7c075913880cf9514de0
 
 const MAKER_PVK = "9e0392c9a2ddffbb2fd26318e00b60dbaf56333b22c0d4e9e9e64ac70ad1e9d1";
 const MAKER_ADDR = "0x3c7ed5834863f6a55946AEfDC079F1141a726982";
@@ -30,10 +30,10 @@ const TAKER_ADDR = "0x3A63DfB819f43Bc7a078F544a75fb5F40e632434";
 
 // const MAKER_PVK = "e402f220a754d0624907961cb733e6876aab8e87dfeb6607a76f22059a99a928";
 // const MAKER_ADDR = "0x3A63DfB819f43Bc7a078F544a75fb5F40e632434";
-// PROVIDER = "https://matic-mumbai.chainstacklabs.com";
 const PROVIDER = "https://goerli.infura.io/v3/13bbb90e81d346c49bc1037e514b715a";
 
 const CHAIN_ID = 5;
+
 describe('NFTSwapV4', () => {
   it('utility functions on class work properly with erc721 sell order', async () => {
     const provider = new ethers.providers.JsonRpcProvider(PROVIDER)
@@ -49,7 +49,7 @@ describe('NFTSwapV4', () => {
 
     const ONE_RXT: UserFacingERC20AssetDataSerializedV4 = {
       tokenAddress: "0x58E9E5dA59199F702683c0237da1822Be3940678", // RXT goerli contract address
-      amount: '1000000', // 1 USDT
+      amount: '10000000', // 1 RXT
       type: 'ERC20',
     };
 
@@ -79,12 +79,12 @@ describe('NFTSwapV4', () => {
       ONE_RXT, // Taker asset to swap
       walletAddressMaker, // Maker address
       {
-        // fees: [
-        //     {
-        //         amount: '10000000000000000', // 0.01 USDT fee
-        //         recipient: '0xe917063Ea98d736cf2cd62efF1AeF116A79CA248', // test 3 address 
-        //     },
-        // ],
+        fees: [
+          {
+            amount: '1000000', // 0.01 RXT fee
+            recipient: '0xe917063Ea98d736cf2cd62efF1AeF116A79CA248', // test 3 address 
+          },
+        ],
       }
     );
     console.log({ order })
@@ -93,23 +93,32 @@ describe('NFTSwapV4', () => {
 
     console.log({ signedOrder })
 
-    // const postedOrder = await nftSwapSdk.postOrder(signedOrder, chainId.toString());
+    // onchain order
+    // const onchainOrderTx = await nftSwapSdk.presignOrder(order);
+    // await onchainOrderTx.tx.wait()
+
+    const onchainOrderStatus = await nftSwapSdk.getOrderStatus(order);
+    console.log({ onchainOrderStatus })
+
+    // hosted order
+    // const postedOrder = await nftSwapSdk.postOrder(signedOrder, CHAIN_ID.toString());
     // console.log({ postedOrder })
 
-    // const fetchedOrder = await nftSwapSdk.getOrders({ nonce: signedOrder.nonce })
+    // const fetchedOrder = await nftSwapSdk.getOrders({ nonce: signedOrder.nonce.toString() })
     // console.log({ fetchedOrder: fetchedOrder.orders })
 
-    // const canceledOrder = await nftSwapSdk.cancelOrder(signedOrder.nonce);
+    // const canceledOrder = await nftSwapSdk.cancelOrder(signedOrder.nonce, 'ERC721');
     // console.log({ canceledOrder })
 
     /* [Part 2: Taker that wants to buy the punk fills trade] */
+    // return;
     console.log("Part 2...")
     const signerForTaker = new ethers.Wallet(TAKER_PVK, provider);
 
     nftSwapSdk = new NftSwapV4(provider, signerForTaker, CHAIN_ID);
 
     const walletAddressTaker = TAKER_ADDR;
-    console.log({walletAddressTaker})
+    console.log({ walletAddressTaker })
 
     const approvalStatusForTaker = await nftSwapSdk.loadApprovalStatus(
       ONE_RXT,
